199c28ebfa02fac32d4d32abcdb2db9d2d017293
Maxim Bubnov <maglighter@gmail.com>
2013-03-27 21:45:36 +0400

    some changes

---
+++ elpa/queue-0.1/queue-autoloads.el
@@ -0,0 +1,25 @@
+;;; queue-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "queue" "queue.el" (20818 5126 960883 761000))
+;;; Generated autoloads from queue.el
+
+(defalias 'make-queue 'queue-create "\
+Create an empty queue data structure.")
+
+;;;***
+
+;;;### (autoloads nil nil ("queue-pkg.el") (20818 5127 55467 340000))
+
+;;;***
+
+(provide 'queue-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; queue-autoloads.el ends here
